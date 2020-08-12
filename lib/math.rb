1268cf1bcbee16d83c52bd3eebb8f2e4a3e0dc3f
confident-architect-8379 <s.kanagui@gmail.com>
2020-08-12 20:20:57 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,11 +9,13 @@ puts difference
 end
 
 def division(num1, num2)
-
+quotient = num1 / num2
+puts quotient
 end
 
 def multiplication(num1, num2)
-
+product = num1 * num2
+puts pruduct 
 end
 
 def modulo(num1, num2)
